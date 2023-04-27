bb705597e68bd7bc8e123aeacc65f1d90525355e
Julio Gonzalez Daher <julio.daher3@gmail.com>
2023-04-27 19:58:51 -0300

    inicio

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+import img from "./logo.png";
+
+const Header = (props) => {
+  // console.log(props);
+  return (
+    <div className="Header">
+      <img src={img} alt="logo chinpokomon" />
+      <h1>{props.title}</h1>
+      
+      <h2>{props.subTitle}</h2>
+    </div>
+  );
+};
+
+export default Header;
