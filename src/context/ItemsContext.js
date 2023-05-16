b3505d561cfac9d6f09b0bcfd45c83c365a237f8
Julio Gonzalez Daher <julio.daher3@gmail.com>
2023-05-15 23:39:00 -0300

    arreglado numero en cart

--- src/context/ItemsContext.js
+++ src/context/ItemsContext.js
@@ -2,24 +2,16 @@ import React, { useState, createContext } from 'react';
 
 export const ItemsContext = createContext();
 
-const initialState = () => [
-	{
-		id: 45454, 
-		name: 'Lambtron',
-		desc: 'Rashitos',
-		price: 4,
-		type: 'electrico',
-	},
-];
+const initialState = () => [];
 
 export const ItemsProvider = ({ children }) => {
-	const [items, setItems] = useState([initialState]);
+    const [items, setItems] = useState(initialState());
 
-	return (
-		<ItemsContext.Provider value={[items, setItems]}>
-			{children}
-		</ItemsContext.Provider>
-	);
+    return (
+        <ItemsContext.Provider value={[items, setItems]}>
+            {children}
+        </ItemsContext.Provider>
+    );
 };
 
 export default ItemsContext;
